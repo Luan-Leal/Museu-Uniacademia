ad9e2391fbd048cbc7b841ee9eb5b6d49f5feec3
Luan <luanleal05@gmail.com>
2022-11-15 17:30:26 -0300

    feat: project finished

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -4,36 +4,29 @@ import Header from "../../components/header/header";
 import CarouselApp from "../../components/header/carousel/carousel";
 import icone from "../../assets/icone.png";
 const Home = () => {
-
-
-
-  
   return (
     <div className="homePage">
       <section>
         <Header />
         <div className="image">
           <div>
-            <h1>Museu Universitário <span>Academia</span> </h1>
+            <h1>
+              Museu Universitário <span>Academia</span>{" "}
+            </h1>
             <img draggable="false" src={icone} alt="icone"></img>
-            
-
           </div>
-          <h1 className="title">Museu de Etnologia Indígena e História Natural  </h1> 
-     
+          <h1 className="title">
+            Museu de Etnologia Indígena e História Natural{" "}
+          </h1>
         </div>
         <main>
           <div>
             <div className="container">
-            <h1>Algumas de nossas Exposições</h1>
-
-            <CarouselApp/>
+              <h1>Algumas de nossas Exposições</h1>
             </div>
-
           </div>
-         
         </main>
-        {/* <CarouselApp/> */}
+        <CarouselApp />
       </section>
     </div>
   );
--- src/views/visit/visit.jsx
+++ src/views/visit/visit.jsx
@@ -1,79 +1,91 @@
+import "./visit.css";
+import React from "react";
+import { useState, useEffect, useRef } from "react";
 import Header from "../../components/header/header";
-import "./visit.scss";
-import Fachada from "../../assets/fachada.jpg"
-
 
 const Visit = () => {
-  return (
-    <div className="visitPage">
-      <section>
-        <Header />
-        <section className='container'>
-      <div className="header-img">
-        <img src={Fachada} className='fachada' alt="Fachada do colegio academia" />
-        <div className="info">
-          <div className="horario">
-            <div className="icon">
-              <i className="fa-regular fa-clock"></i>
-            </div>
+  const carousel = useRef(null);
 
-          <h1>Horário</h1>
-            <p>
-              De segunda a sexta-feira, das 13h às 17h.
-            </p>
-            <p>
-              Fechado sábados, domingos e feriados 
-            </p>
+  const list = [
+    {
+      id: "11111",
+      title: "Alguns animais em destaque",
+      image:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/68/6b/dd/museu-de-historia-natural.jpg?w=2000&h=-1&s=1",
+    },
+    {
+      id: "22222",
+      title: "Conchas",
+      image:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/68/6c/7d/museu-de-historia-natural.jpg?w=2000&h=-1&s=1",
+    },
+
+    {
+      id: "33333",
+      title: "Arara Azul",
+      image:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/68/6c/35/museu-de-historia-natural.jpg?w=2000&h=-1&s=1",
+    },
+    {
+      id: "44444",
+      title: "Raposa",
+      image:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/68/6b/5d/museu-de-historia-natural.jpg?w=2000&h=-1&s=1",
+    },
+    {
+      id: "55555",
+      title: "Pintura de índios",
+      image:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/1d/97/60/adorei-lugar-pequeno.jpg?w=2200&h=-1&s=1",
+    },
+    {
+      id: "66666",
+      title: "Quadro exibindo a classe curstacea",
+      image:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/68/6c/ae/museu-de-historia-natural.jpg?w=2000&h=-1&s=1",
+    },
+  ];
+
+  const handleLeftClick = (event) => {
+    event.preventDefault();
+    console.log(carousel.current.offsetWidth);
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (event) => {
+    event.preventDefault();
+    console.log(carousel.current.offsetWidth);
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  return (
+    <section id="CarouselSection">
+      <Header />
+      <div className="body">
+        <div className="header">
+          <div>
+            <h1>Algumas Peças</h1>
           </div>
-          
-          <div className="contato">
-            <div className="icon">
-              <i className="fa-solid fa-arrow-right"></i>
-            </div>
-            <h1>Onde estamos</h1>
-            <p>Rua Halfeld, 1179 - Centro - CEP 36.016-000 - Juiz de Fora - MG</p>
-            <p>
-              <a href="https://www.google.com.br/maps/dir/-21.7757602,-43.3970179/UniAcademia+(CES%2FJF)+-+Campus+Academia+-+R.+Halfeld,+1.179+-+Centro,+Juiz+de+Fora+-+MG,+36016-000/@-21.7651847,-43.3798992,15z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x989b61e5bb4d59:0x8a1d7c0df90967f6!2m2!1d-43.3530921!2d-21.7625812" target="_blank"> 
-              Check o Google Maps</a>
-            </p>
-          </div>
-                      
-          <div className="entrada">
-            <div className="icon">
-              <i className="fa-solid fa-ticket"></i>
-            </div>
-            <h1>Entrada</h1>
-            <p>Entrada é <span>gratuita</span></p>
-          </div>
+        </div>
+        <div className="carousel" ref={carousel}>
+          {list.map((item) => {
+            const { id, title, image } = item;
+            return (
+              <div className="item" key={id}>
+                <img className="img-article" src={image} alt={title} />
+                <div className="titleCarousel">
+                  <span>{title}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="buttons">
+          <button onClick={handleLeftClick}>&#8592;</button>
+          <button onClick={handleRightClick}>&rarr;</button>
         </div>
       </div>
     </section>
-
-    <section className="guia">
-    <div className="guia-titulo">
-        <h1>Guia de Visita</h1>
-    </div>
-    <div className="guia-paragrafo">
-        <p>Seguindo as indicações da Prefeitura de Juiz de Fora (PJF), a partir desta quarta-feira, dia 8 de junho, volta a ser obrigatório o uso de máscaras em instituições <br/> de ensino. Logo, a partir dessa data, será exigido o uso de máscaras no UniAcademia, Colégio Academia e o Museu Universitário Academia. <br/> Isso vale para as salas de aula, laboratórios, banheiros, corredores e qualquer outro ambiente dos campus.
-        </p>
-        <p>A volta se dá após o crescimento do número de casos e de contaminações por Covid-19, confirmados pelo monitoramento do cenário epidemiológico.
-        </p>    
-        <p>Por isso, mantenha-se seguro com o uso da máscara cobrindo o rosto e do álcool em gel.</p>
-    </div>
-    </section>
-
-    <section>
-        <div className="redes-sociais">
-            <h1>Visite as redes sociais do Museu Academia</h1>
-            <div className="social-list">
-                <div className="instagram"><i className="fa-brands fa-instagram"></i></div>
-                <div className="facebook"><i className="fa-brands fa-facebook"></i></div>
-                <div className="youtube"><i className="fa-brands fa-youtube"></i></div>
-            </div>
-        </div>
-    </section>
-      </section>
-    </div>
   );
 };
 export default Visit;
